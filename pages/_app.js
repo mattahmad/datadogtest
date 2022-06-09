f13b24ba17dac39d09f70954375098bf9b009adf
Matt Ahmad <mattahmad@Matt-Ahmad-MP0-288.local>
2022-06-08 20:37:51 -0400

    testing datadog instrumentation - update _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import '../styles/globals.css'
 
+
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '34479ab2-bd3a-4552-b2c3-1aa4b40b966f',
+    clientToken: 'pub9c5479042679bcb961fe0064ffd0cbf3',
+    site: 'datadoghq.com',
+    service:'datadog-rum-test',
+    env:'prod',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    premiumSampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
+
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,27 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 
-import { datadogRum } from '@datadog/browser-rum';
-
-datadogRum.init({
-    applicationId: '34479ab2-bd3a-4552-b2c3-1aa4b40b966f',
-    clientToken: 'pub9c5479042679bcb961fe0064ffd0cbf3',
-    site: 'datadoghq.com',
-    service:'datadog-rum-test',
-    env:'prod',
-    // Specify a version number to identify the deployed version of your application in Datadog 
-    // version: '1.0.0',
-    sampleRate: 100,
-    premiumSampleRate: 100,
-    trackInteractions: true,
-    defaultPrivacyLevel:'mask-user-input'
-});
-    
-datadogRum.startSessionReplayRecording();
-
-
-
-
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
